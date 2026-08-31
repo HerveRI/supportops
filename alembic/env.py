653923ae0fba2b6b42827d10ea1a9d23db21d7cd
HerveRI <herve.irad@gmail.com>
2026-08-30 23:46:07 -0400

    User model set-up

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine, pool
 from alembic import context
 from app.config import settings
 from app.db.base import Base
+from app.models.user import User  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
